5fe6c6ebe01b3134556d51c92ecdb2c2d904429b
MAlex <alexmaximovj@gmail.com>
2015-05-05 14:18:22 +0300

    Initial commit

---
+++ src/LecturePHP.java
@@ -0,0 +1,24 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Alex on 04.05.2015.
+ */
+public class LecturePHP {
+
+    private Map<Integer, Student> mapStudent = new HashMap<>();
+
+
+    public Collection<Student> getStudentList() {
+        return mapStudent.values();
+    }
+
+    public void setMapStudent(Integer id, Student student) {
+        if (id < 0 & student == null) {
+            throw new IllegalArgumentException(">>>>>>>>>> public void setMapStudent(Integer id, Student student) ->>>>  id < 0 & student == null ");
+        }
+
+        this.mapStudent.put(id, student);
+    }
+}
---
+++ src/TestMainJSON.java
@@ -0,0 +1,27 @@
+import com.google.gson.Gson;
+
+/**
+ * Created by Alex on 04.05.2015.
+ */
+public class TestMainJSON {
+
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Student student = new Student(1, "Vasja", 23);
+        System.out.println(">>>>>   Object -> Student");
+        System.out.println(student);
+
+
+        //toJSON
+        System.out.println(">>>>>   toJSON");
+        String json = gson.toJson(student);
+        System.out.println(json);
+
+        //fromJSON
+        System.out.println("/>>>>   fromJSON");
+        Student studentJSON = gson.fromJson(json, Student.class);
+        System.out.println(studentJSON);
+
+
+    }
+}
